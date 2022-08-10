c704b449c5bec8ce5747eef5155f4749f0f59f85
Edward NoaLand <edwardnoaland@gmail.com>
2022-08-10 14:56:22 +0800

    refactor: rename handle_list to closure, since this function can handle function as well

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@ std::istream& is{std::cin};
 Token_stream ts{is};
 
 void scheme(Token_stream& ts);
-s_expression * function(Token_stream& ts);
-s_expression* handle_list(Token_stream& ts);
+s_expression* function(Token_stream& ts);
+s_expression* closure(Token_stream& ts);
 
 int main() {
     std::cout << "Let's start!" << std::endl;
@@ -30,13 +30,10 @@ void scheme(Token_stream& ts) {
                 continue;
             }
             case '(': {
-                // actually, not only s_exp will start with (, almost everything in Scheme can be started with it, which means I need to match this with term
-                // 1. s_exp
                 ts.put_back(token);
-                s_expression* s_exp = handle_list(ts);
+                s_expression* s_exp = closure(ts);
                 s_exp->print(std::cout);
                 continue;
-                // 2. other expression, so this version of code cannot run code like: car (car ((a))), since it will take (car ((a))) as list, but find `car` inside it
             }
             case ')':
                 continue;
@@ -51,7 +48,7 @@ s_expression* function(Token_stream& ts) {
     Token func = ts.get();
     std::string &f = func.value;
     if(f == "car") {
-        s_expression* l = handle_list(ts);
+        s_expression* l = closure(ts);
         if(l->get_indicator() != "list") {
             throw std::runtime_error("wrong syntax, car can only get list.");
         }
@@ -62,7 +59,7 @@ s_expression* function(Token_stream& ts) {
     }
 }
 
-s_expression* handle_list(Token_stream& ts) {
+s_expression* closure(Token_stream& ts) {
     const Token &left = ts.get();
     if(left.type != '(') {
         throw std::runtime_error("wrong syntax: " + left.value);
@@ -78,7 +75,7 @@ s_expression* handle_list(Token_stream& ts) {
             break;
         } else if(token.type == '(') {
             ts.put_back(token);
-            list* pList = (list*)handle_list(ts);
+            list* pList = (list*) closure(ts);
             l->push_back(pList);
         } else if(token.type == 'F') {
             ts.put_back(token);
