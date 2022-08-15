94ce1146dd55a0bd373a9b0dc7e5e9f3bd55dc38
Edward NoaLand <edwardnoaland@gmail.com>
2022-08-15 15:51:06 +0800

    refactor: move param type check in constructor for math family

--- token/function/math_family.h
+++ token/function/math_family.h
@@ -6,7 +6,11 @@
 
 class self_add: public function {
 public:
-    self_add(s_expression* n): number(n) {}
+    self_add(s_expression* n): number(n) {
+        if(number->get_indicator() != "integer") {
+            throw std::runtime_error("wrong syntax: add1 need integer as param!");
+        }
+    }
     integer* execute() override;
     std::string return_type() override { return "integer"; }
     std::string name() override { return "add1"; }
@@ -16,7 +20,11 @@ private:
 
 class self_sub: public function {
 public:
-    self_sub(s_expression* n): number(n) {}
+    self_sub(s_expression* n): number(n) {
+        if(number->get_indicator() != "integer") {
+            throw std::runtime_error("wrong syntax: sub1 need integer as param!");
+        }
+    }
     integer* execute() override;
     std::string return_type() override { return "integer"; }
     std::string name() override { return "sub1"; }
@@ -26,10 +34,14 @@ private:
 
 class is_zero: public function {
 public:
-    is_zero(s_expression* n): number(n) {}
+    is_zero(s_expression* n): number(n) {
+        if(number->get_indicator() != "integer") {
+            throw std::runtime_error("wrong syntax: zero? need integer as param!");
+        }
+    }
     boolean* execute() override;
     std::string return_type() override { return "bool"; }
-    std::string name() override { return "is_zero"; }
+    std::string name() override { return "zero?"; }
 private:
     s_expression* number;
 };
