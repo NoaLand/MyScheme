312ce95a2cf5377909f0a49b7b7eff179ba83ece
Edward NoaLand <edwardnoaland@gmail.com>
2022-08-25 17:55:16 +0800

    refactor: change get_value result name in test cases

--- tests/s_expression/atom_test.cpp
+++ tests/s_expression/atom_test.cpp
@@ -4,32 +4,32 @@
 
 TEST(AtomTest, should_get_value_successfully_after_create_atom) {
     auto a = new atom{"value_of_atom"};
-    const std::string& atom_value = a->get_value();
+    const std::string& atom_to_string = a->get_value();
     const std::string& atom_indicator = a->get_indicator();
 
-    ASSERT_EQ(atom_value, "value_of_atom");
+    ASSERT_EQ(atom_to_string, "value_of_atom");
     ASSERT_EQ(atom_indicator, "atom");
 }
 
 TEST(BooleanTest, should_set_true_successfully_when_pass_bool_true) {
     auto b = new boolean{true};
     bool val = b->val();
-    const std::string& boolean_value = b->get_value();
+    const std::string& boolean_to_string = b->get_value();
     const std::string& boolean_indicator = b->get_indicator();
 
     ASSERT_EQ(val, true);
-    ASSERT_EQ(boolean_value, "#t");
+    ASSERT_EQ(boolean_to_string, "#t");
     ASSERT_EQ(boolean_indicator, "bool");
 }
 
 TEST(BooleanTest, should_set_false_successfully_when_pass_bool_false) {
     auto b = new boolean{false};
     bool val = b->val();
-    const std::string& boolean_value = b->get_value();
+    const std::string& boolean_to_string = b->get_value();
     const std::string& boolean_indicator = b->get_indicator();
 
     ASSERT_EQ(val, false);
-    ASSERT_EQ(boolean_value, "#f");
+    ASSERT_EQ(boolean_to_string, "#f");
     ASSERT_EQ(boolean_indicator, "bool");
 }
 
@@ -37,11 +37,11 @@ TEST(BooleanTest, should_set_true_when_pass_string_t) {
     const std::string true_string = "#t";
     auto b = new boolean{true_string};
     bool val = b->val();
-    const std::string& boolean_value = b->get_value();
+    const std::string& boolean_to_string = b->get_value();
     const std::string& boolean_indicator = b->get_indicator();
 
     ASSERT_EQ(val, true);
-    ASSERT_EQ(boolean_value, "#t");
+    ASSERT_EQ(boolean_to_string, "#t");
     ASSERT_EQ(boolean_indicator, "bool");
 }
 
@@ -49,11 +49,11 @@ TEST(BooleanTest, should_set_true_when_pass_else) {
     const std::string else_string = "else";
     auto b = new boolean{else_string};
     bool val = b->val();
-    const std::string& boolean_value = b->get_value();
+    const std::string& boolean_to_string = b->get_value();
     const std::string& boolean_indicator = b->get_indicator();
 
     ASSERT_EQ(val, true);
-    ASSERT_EQ(boolean_value, "#t");
+    ASSERT_EQ(boolean_to_string, "#t");
     ASSERT_EQ(boolean_indicator, "bool");
 }
 
@@ -61,11 +61,11 @@ TEST(BooleanTest, should_set_false_when_pass_string_f) {
     const std::string false_string = "#f";
     auto b = new boolean{false_string};
     bool val = b->val();
-    const std::string& boolean_value = b->get_value();
+    const std::string& boolean_to_string = b->get_value();
     const std::string& boolean_indicator = b->get_indicator();
 
     ASSERT_EQ(val, false);
-    ASSERT_EQ(boolean_value, "#f");
+    ASSERT_EQ(boolean_to_string, "#f");
     ASSERT_EQ(boolean_indicator, "bool");
 }
 
