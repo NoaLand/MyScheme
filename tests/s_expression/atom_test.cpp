533879761146ef6f5b07dbd83960b3b8957a17d9
Edward NoaLand <edwardnoaland@gmail.com>
2022-08-25 17:26:14 +0800

    format: format code

--- tests/s_expression/atom_test.cpp
+++ tests/s_expression/atom_test.cpp
@@ -6,6 +6,7 @@ TEST(AtomTest, should_get_value_successfully_after_create_atom) {
     auto a = new atom{"value_of_atom"};
     const std::string& atom_value = a->get_value();
     const std::string& atom_indicator = a->get_indicator();
+    
     ASSERT_EQ(atom_value, "value_of_atom");
     ASSERT_EQ(atom_indicator, "atom");
 }
