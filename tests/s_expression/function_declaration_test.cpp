4ef112d6ef16f073afd2b36dc2ac2a20a8eea12e
Edward NoaLand <edwardnoaland@gmail.com>
2022-08-25 18:22:28 +0800

    test: add test cases for function_context

--- tests/s_expression/function_declaration_test.cpp
+++ tests/s_expression/function_declaration_test.cpp
@@ -18,3 +18,26 @@ TEST(FunctionDeclarationTest, should_get_info_of_lat_function_successfully_after
     ASSERT_EQ(lat->get_params()->get(0)->get_value(), "l");
     ASSERT_EQ(lat->get_body(), lat_body);
 }
+
+TEST(FunctionContextTest, should_return_true_when_function_has_been_stored_in_context) {
+    function_context context;
+
+    auto lat_params = new list<param>();
+    lat_params->push_back(new param{"l"});
+    std::string lat_body = "(cond ( ( null? $l$ ) #t ) ( ( atom? ( car $l$ ) ) ( lat? ( cdr $l$ ) ) ) ( else #f ) )";
+    auto lat = new function_declaration(
+            "lat?",
+            lat_params,
+            lat_body
+    );
+
+    context.store(lat);
+
+    ASSERT_EQ(context.is_in("lat?"), true);
+}
+
+TEST(FunctionContextTest, should_return_false_when_function_is_not_in_context) {
+    function_context context;
+
+    ASSERT_EQ(context.is_in("lat?"), false);
+}
