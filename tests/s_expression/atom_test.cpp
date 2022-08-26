1808a4eca65f29ac6728eccc50c0caa76686be22
Edward NoaLand <edwardnoaland@gmail.com>
2022-08-26 14:09:27 +0800

    test: add atom, boolean, integer -> print function testcases

--- tests/s_expression/atom_test.cpp
+++ tests/s_expression/atom_test.cpp
@@ -4,69 +4,62 @@
 
 TEST(AtomTest, should_get_value_successfully_after_create_atom) {
     auto a = new atom{"value_of_atom"};
-    const std::string& atom_to_string = a->get_value();
-    const std::string& atom_indicator = a->get_indicator();
+    ASSERT_EQ(a->get_value(), "value_of_atom");
+    ASSERT_EQ(a->get_indicator(), "atom");
 
-    ASSERT_EQ(atom_to_string, "value_of_atom");
-    ASSERT_EQ(atom_indicator, "atom");
+    std::ostringstream buf;
+    a->print(buf);
+    ASSERT_EQ(buf.str(), "atom: value_of_atom\n");
 }
 
 TEST(BooleanTest, should_set_true_successfully_when_pass_bool_true) {
     auto b = new boolean{true};
-    bool val = b->val();
-    const std::string& boolean_to_string = b->get_value();
-    const std::string& boolean_indicator = b->get_indicator();
+    ASSERT_EQ(b->val(), true);
+    ASSERT_EQ(b->get_value(), "#t");
+    ASSERT_EQ(b->get_indicator(), "bool");
 
-    ASSERT_EQ(val, true);
-    ASSERT_EQ(boolean_to_string, "#t");
-    ASSERT_EQ(boolean_indicator, "bool");
+    std::ostringstream buf;
+    b->print(buf);
+    ASSERT_EQ(buf.str(), "bool: #t\n");
 }
 
 TEST(BooleanTest, should_set_false_successfully_when_pass_bool_false) {
     auto b = new boolean{false};
-    bool val = b->val();
-    const std::string& boolean_to_string = b->get_value();
-    const std::string& boolean_indicator = b->get_indicator();
 
-    ASSERT_EQ(val, false);
-    ASSERT_EQ(boolean_to_string, "#f");
-    ASSERT_EQ(boolean_indicator, "bool");
+    ASSERT_EQ(b->val(), false);
+    ASSERT_EQ(b->get_value(), "#f");
+    ASSERT_EQ(b->get_indicator(), "bool");
+
+    std::ostringstream buf;
+    b->print(buf);
+    ASSERT_EQ(buf.str(), "bool: #f\n");
 }
 
 TEST(BooleanTest, should_set_true_when_pass_string_t) {
     const std::string true_string = "#t";
     auto b = new boolean{true_string};
-    bool val = b->val();
-    const std::string& boolean_to_string = b->get_value();
-    const std::string& boolean_indicator = b->get_indicator();
 
-    ASSERT_EQ(val, true);
-    ASSERT_EQ(boolean_to_string, "#t");
-    ASSERT_EQ(boolean_indicator, "bool");
+    ASSERT_EQ(b->val(), true);
+    ASSERT_EQ(b->get_value(), "#t");
+    ASSERT_EQ(b->get_indicator(), "bool");
 }
 
 TEST(BooleanTest, should_set_true_when_pass_else) {
     const std::string else_string = "else";
     auto b = new boolean{else_string};
-    bool val = b->val();
-    const std::string& boolean_to_string = b->get_value();
-    const std::string& boolean_indicator = b->get_indicator();
 
-    ASSERT_EQ(val, true);
-    ASSERT_EQ(boolean_to_string, "#t");
-    ASSERT_EQ(boolean_indicator, "bool");
+    ASSERT_EQ(b->val(), true);
+    ASSERT_EQ(b->get_value(), "#t");
+    ASSERT_EQ(b->get_indicator(), "bool");
 }
 
 TEST(BooleanTest, should_set_false_when_pass_string_f) {
     const std::string false_string = "#f";
     auto b = new boolean{false_string};
-    bool val = b->val();
-    const std::string& boolean_to_string = b->get_value();
-    const std::string& boolean_indicator = b->get_indicator();
 
-    ASSERT_EQ(val, false);
-    ASSERT_EQ(boolean_to_string, "#f");
-    ASSERT_EQ(boolean_indicator, "bool");
+    ASSERT_EQ(b->val(), false);
+    ASSERT_EQ(b->get_value(), "#f");
+    ASSERT_EQ(b->get_indicator(), "bool");
 }
 
 TEST(BooleanTest, should_throw_exception_when_input_is_not_t_or_else_or_f) {
@@ -76,11 +69,11 @@ TEST(BooleanTest, should_throw_exception_when_input_is_not_t_or_else_or_f) {
 
 TEST(IntegerTest, should_set_10_to_integer_when_passing_10) {
     auto i = new integer{10};
-    auto val = i->val();
-    const std::string& int_to_string = i->get_value();
-    const std::string& int_indicator = i->get_indicator();
+    ASSERT_EQ(i->val(), 10);
+    ASSERT_EQ(i->get_value(), "10");
+    ASSERT_EQ(i->get_indicator(), "integer");
 
-    ASSERT_EQ(val, 10);
-    ASSERT_EQ(int_to_string, "10");
-    ASSERT_EQ(int_indicator, "integer");
+    std::ostringstream buf;
+    i->print(buf);
+    ASSERT_EQ(buf.str(), "integer: 10\n");
 }
