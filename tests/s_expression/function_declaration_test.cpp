13c6b99ca3db141ecc8726e2f0fcbfa290578db7
Edward NoaLand <edwardnoaland@gmail.com>
2022-08-26 14:36:19 +0800

    refactor: use testfixture to setup predefined lat function before all

--- tests/s_expression/function_declaration_test.cpp
+++ tests/s_expression/function_declaration_test.cpp
@@ -2,16 +2,24 @@
 
 #include "s_expression/function_declaration.h"
 
-TEST(FunctionDeclarationTest, should_get_info_of_lat_function_successfully_after_init_function) {
-    auto lat_params = new list<param>();
-    lat_params->push_back(new param{"l"});
-    std::string lat_body = "(cond ( ( null? $l$ ) #t ) ( ( atom? ( car $l$ ) ) ( lat? ( cdr $l$ ) ) ) ( else #f ) )";
-    auto lat = new function_declaration(
-            "lat?",
-            lat_params,
-            lat_body
-    );
 
+class FunctionDeclarationAndContextTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        auto lat_params = new list<param>();
+        lat_params->push_back(new param{"l"});
+        lat = std::make_shared<function_declaration>(
+                "lat?",
+                lat_params,
+                lat_body
+        );
+    }
+
+    std::shared_ptr<function_declaration> lat{};
+    const std::string lat_body = "(cond ( ( null? $l$ ) #t ) ( ( atom? ( car $l$ ) ) ( lat? ( cdr $l$ ) ) ) ( else #f ) )";
+};
+
+TEST_F(FunctionDeclarationAndContextTest, should_get_info_of_lat_function_successfully_after_init_function) {
     std::string function_definition_body = "-> name: lat?\n-> var: ( l )\n-> body: (cond ( ( null? $l$ ) #t ) ( ( atom? ( car $l$ ) ) ( lat? ( cdr $l$ ) ) ) ( else #f ) )";
 
     ASSERT_EQ(lat->get_indicator(), "customized_function");
@@ -26,42 +34,22 @@ TEST(FunctionDeclarationTest, should_get_info_of_lat_function_successfully_after
     ASSERT_EQ(buf.str(), "---- func ----\n" + function_definition_body + "\n");
 }
 
-TEST(FunctionContextTest, should_return_true_when_function_has_been_stored_in_context) {
+TEST_F(FunctionDeclarationAndContextTest, should_return_true_when_function_has_been_stored_in_context) {
     function_context context;
-
-    auto lat_params = new list<param>();
-    lat_params->push_back(new param{"l"});
-    std::string lat_body = "(cond ( ( null? $l$ ) #t ) ( ( atom? ( car $l$ ) ) ( lat? ( cdr $l$ ) ) ) ( else #f ) )";
-    auto lat = new function_declaration(
-            "lat?",
-            lat_params,
-            lat_body
-    );
-
-    context.store(lat);
+    context.store(lat.get());
 
     ASSERT_EQ(context.is_in("lat?"), true);
 }
 
-TEST(FunctionContextTest, should_return_false_when_function_is_not_in_context) {
+TEST_F(FunctionDeclarationAndContextTest, should_return_false_when_function_is_not_in_context) {
     function_context context;
 
     ASSERT_EQ(context.is_in("lat?"), false);
 }
 
-TEST(FunctionContextTest, should_throw_exception_when_store_function_already_exist) {
+TEST_F(FunctionDeclarationAndContextTest, should_throw_exception_when_store_function_already_exist) {
     function_context context;
+    context.store(lat.get());
 
-    auto lat_params = new list<param>();
-    lat_params->push_back(new param{"l"});
-    std::string lat_body = "(cond ( ( null? $l$ ) #t ) ( ( atom? ( car $l$ ) ) ( lat? ( cdr $l$ ) ) ) ( else #f ) )";
-    auto lat = new function_declaration(
-            "lat?",
-            lat_params,
-            lat_body
-    );
-
-    context.store(lat);
-
-    ASSERT_ANY_THROW(context.store(lat));
+    ASSERT_ANY_THROW(context.store(lat.get()));
 }
