0a5f9e3f6b3dcad16c4c8c2a470ae68006219d61
Edward NoaLand <edwardnoaland@gmail.com>
2022-08-26 15:54:53 +0800

    test: add test for car execute

--- tests/function/list_family_test.cpp
+++ tests/function/list_family_test.cpp
@@ -16,3 +16,19 @@ TEST(CarTest, should_get_correct_basic_info_after_successfully_init_car_expressi
     ASSERT_EQ(f->return_type(), "s_expression");
     ASSERT_EQ(f->family(), "list");
 }
+
+TEST(CarTest, should_return_a1_when_car_get_a1_a2_a3_list) {
+    auto l = new list<atom>{};
+    auto a1 = new atom{"a1"};
+    auto a2 = new atom{"a2"};
+    auto a3 = new atom{"a3"};
+    l->push_back(a1);
+    l->push_back(a2);
+    l->push_back(a3);
+
+    auto f = new car{l};
+    auto res = f->execute();
+
+    ASSERT_EQ(res->get_indicator(), "atom");
+    ASSERT_EQ(res->get_value(), "a1");
+}
