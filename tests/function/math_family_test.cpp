f5da9fc557e67d4d4ca0cbf204d92c996cb4d066
Edward NoaLand <edwardnoaland@gmail.com>
2022-08-27 18:23:37 +0800

    test: add test cases for self_sub, since self_sub is similar to self_add

--- tests/function/math_family_test.cpp
+++ tests/function/math_family_test.cpp
@@ -36,3 +36,38 @@ TEST(SelfAddTest, should_throw_exception_when_param_passing_is_not_integer) {
 
     ASSERT_ANY_THROW(new self_add{a});
 }
+
+TEST(SelfSubTest, should_get_correct_basic_info_when_successfully_create_self_sub_expression) {
+    auto i = new integer{1};
+    auto f = new self_sub{i};
+
+    ASSERT_EQ(f->name(), "sub1");
+    ASSERT_EQ(f->return_type(), "integer");
+    ASSERT_EQ(f->family(), "math");
+}
+
+TEST(SelfSubTest, should_return_1_when_self_sub_get_2_as_input) {
+    auto i = new integer{2};
+
+    auto f = new self_sub{i};
+    auto res = f->execute();
+
+    ASSERT_EQ(res->get_indicator(), "integer");
+    ASSERT_EQ(res->val(), 1);
+}
+
+TEST(SelfSubTest, should_return_100_when_self_sub_get_101_as_input) {
+    auto i = new integer{101};
+
+    auto f = new self_sub{i};
+    auto res = f->execute();
+
+    ASSERT_EQ(res->get_indicator(), "integer");
+    ASSERT_EQ(res->val(), 100);
+}
+
+TEST(SelfSubTest, should_throw_exception_when_param_passing_is_not_integer) {
+    auto a = new atom{"wrong_type_input"};
+
+    ASSERT_ANY_THROW(new self_sub{a});
+}
