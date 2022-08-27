d5b64d49ff2a9ad86db9bc21dec74da3b7d1e780
Edward NoaLand <edwardnoaland@gmail.com>
2022-08-27 18:12:56 +0800

    test: should add 1 when getting an integer

--- tests/function/math_family_test.cpp
+++ tests/function/math_family_test.cpp
@@ -10,3 +10,13 @@ TEST(SelfAddTest, should_get_correct_basic_info_when_successfully_create_self_ad
     ASSERT_EQ(f->return_type(), "integer");
     ASSERT_EQ(f->family(), "math");
 }
+
+TEST(SelfAddTest, should_return_2_when_self_add_get_1_as_input) {
+    auto i = new integer{1};
+
+    auto f = new self_add{i};
+    auto res = f->execute();
+
+    ASSERT_EQ(res->get_indicator(), "integer");
+    ASSERT_EQ(res->val(), 2);
+}
