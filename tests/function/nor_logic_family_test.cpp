0a5da5dc05ab8cc827740b8e4c993fe556820d45
Edward NoaLand <edwardnoaland@gmail.com>
2022-08-29 08:55:27 +0800

    test: should return true when l or r is true in or_logic

--- tests/function/nor_logic_family_test.cpp
+++ tests/function/nor_logic_family_test.cpp
@@ -12,3 +12,14 @@ TEST(OrLogicTest, should_get_correct_basic_info_when_successfully_create_or_logi
     ASSERT_EQ(f->return_type(), "bool");
     ASSERT_EQ(f->family(), "nor_logic");
 }
+
+TEST(OrLogicTest, should_return_true_when_r_or_l_is_true) {
+    auto l = new boolean{true};
+    auto r = new boolean{false};
+
+    auto f = new or_logic{l, r};
+    auto res = f->execute();
+
+    ASSERT_EQ(res->get_indicator(), "bool");
+    ASSERT_EQ(res->val(), true);
+}
