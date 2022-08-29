46420b9f4bc83d47473804066138eec58ad5bb18
Edward NoaLand <edwardnoaland@gmail.com>
2022-08-29 09:21:52 +0800

    test: should return a1 when quote get a car expression

--- tests/function/others_test.cpp
+++ tests/function/others_test.cpp
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
 
+#include "s_expression/list.h"
 #include "function/others.h"
+#include "function/list_family.h"
 
 TEST(QuoteTest, should_get_correct_basic_info_when_successfully_create_quote_expression) {
     auto a = new atom{"a"};
@@ -19,4 +21,23 @@ TEST(QuoteTest, should_return_a_when_quote_atom_a) {
 
     ASSERT_EQ(res->get_indicator(), "atom");
     ASSERT_EQ(res->get_value(), "a");
+}
+
+TEST(QuoteTest, should_return_a1_when_quote_car_a1_a2_a3_list) {
+    auto l = new list<atom>{};
+    auto a1 = new atom{"a1"};
+    auto a2 = new atom{"a2"};
+    auto a3 = new atom{"a3"};
+    l->push_back(a1);
+    l->push_back(a2);
+    l->push_back(a3);
+
+    auto c = new car{l};
+    auto car_res = c->execute();
+
+    auto f = new quote{car_res};
+    auto res = f->execute();
+
+    ASSERT_EQ(res->get_indicator(), "atom");
+    ASSERT_EQ(res->get_value(), "a1");
 }
