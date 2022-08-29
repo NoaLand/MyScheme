75d99d29d011ea955b5811a77e8e1a4861a6ef9c
Edward NoaLand <edwardnoaland@gmail.com>
2022-08-29 11:20:27 +0800

    test: should return 123 and N type when get 123 in buffer

--- tests/token/token_test.cpp
+++ tests/token/token_test.cpp
@@ -24,3 +24,15 @@ TEST(TokenTest, should_throw_exception_when_putback_token_twice_at_the_same_time
 
     ASSERT_ANY_THROW(ts.put_back(t));
 }
+
+TEST(TokenTest, should_return_token_with_N_type_when_getting_an_integer_123) {
+    std::string buf = "123\n";
+    std::istringstream is;
+    is.str(buf);
+    function_context context;
+    Token_stream ts{is, &context};
+
+    const Token &token = ts.get();
+    ASSERT_EQ(token.type, 'N');
+    ASSERT_EQ(token.integer_value, 123);
+}
