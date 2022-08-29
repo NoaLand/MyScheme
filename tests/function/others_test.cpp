08493234f49c0ccb4ab4f605b832e371bf15f5c8
Edward NoaLand <edwardnoaland@gmail.com>
2022-08-29 09:18:24 +0800

    test: should return a when quote atom a

--- tests/function/others_test.cpp
+++ tests/function/others_test.cpp
@@ -10,3 +10,13 @@ TEST(QuoteTest, should_get_correct_basic_info_when_successfully_create_quote_exp
     ASSERT_EQ(f->return_type(), "atom");
     ASSERT_EQ(f->family(), "other");
 }
+
+TEST(QuoteTest, should_return_a_when_quote_atom_a) {
+    auto a = new atom{"a"};
+
+    auto f = new quote{a};
+    auto res = f->execute();
+
+    ASSERT_EQ(res->get_indicator(), "atom");
+    ASSERT_EQ(res->get_value(), "a");
+}
