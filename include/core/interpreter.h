024b259bf1c05b1bdd7ef39077e65f40866a04b0
Edward NoaLand <edwardnoaland@gmail.com>
2022-08-29 15:28:08 +0800

    refactor: extract scheme interpreter to core folder

---
+++ include/core/interpreter.h
@@ -0,0 +1,30 @@
+#ifndef MYSCHEME_INTERPRETER_H
+#define MYSCHEME_INTERPRETER_H
+
+#include "token/token.h"
+#include "s_expression/atom.h"
+
+#include "function/function.h"
+#include "function/list_family.h"
+#include "function/math_family.h"
+#include "function/nor_logic_family.h"
+#include "function/s_expression_family.h"
+#include "function/others.h"
+
+class interpreter {
+public:
+    interpreter(function_context& c, Token_stream& t_stream): context{c}, ts{t_stream} {};
+    auto scheme() -> void;
+    auto function_define() -> function_declaration*;
+    auto construct_from_token() -> s_expression*;
+    auto func() -> s_expression*;
+    auto closure() -> s_expression*;
+    auto get_input_param() -> list<s_expression>*;
+    auto collect_params() -> list<param>*;
+    auto get_func_body(list<param>* params) -> std::string;
+    auto ignore_else() -> void;
+private:
+    function_context& context;
+    Token_stream& ts;
+};
+#endif //MYSCHEME_INTERPRETER_H
