052792c40bbab01fc9ed2325ab52c1211a3be57b
Edward NoaLand <edwardnoaland@gmail.com>
2022-08-29 16:03:58 +0800

    test: add test case for collect_param

--- tests/core/interpreter_test.cpp
+++ tests/core/interpreter_test.cpp
@@ -3,4 +3,15 @@
 #include "core/interpreter.h"
 
 class InterpreterTest: public BaseTest {
+protected:
+    interpreter inter{context, ts};
 };
+
+TEST_F(InterpreterTest, should_get_one_param_successfully_when_passing_one_param_as_input) {
+    is.str("(l)");
+
+    auto params = inter.collect_params();
+
+    ASSERT_EQ(params->size_of(), 1);
+    ASSERT_EQ(params->get(0)->get_value(), "l");
+}
