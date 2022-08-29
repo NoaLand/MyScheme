1ea4db6c7b5f000735b58ea1b619a81a50d16035
Edward NoaLand <edwardnoaland@gmail.com>
2022-08-29 10:18:11 +0800

    test: add test case for preload_libs

---
+++ tests/preload_libs/preload_libs_test.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+
+#include "preload_libs/preload_libs.h"
+
+TEST(PreloadLibsTest, should_contain_loaded_func_when_searching_for_lat) {
+    auto context = function_context{};
+    preload_libs(context);
+
+    ASSERT_TRUE(context.is_in("lat?"));
+    ASSERT_TRUE(context.is_in("length"));
+    ASSERT_TRUE(context.is_in("member?"));
+    ASSERT_TRUE(context.is_in("rember"));
+    ASSERT_TRUE(context.is_in("pick"));
+    ASSERT_TRUE(context.is_in("rempick"));
+    ASSERT_TRUE(context.is_in("+"));
+    ASSERT_TRUE(context.is_in("-"));
+    ASSERT_TRUE(context.is_in("*"));
+    ASSERT_TRUE(context.is_in("/"));
+    ASSERT_TRUE(context.is_in(">"));
+    ASSERT_TRUE(context.is_in("<"));
+    ASSERT_TRUE(context.is_in("="));
+    ASSERT_TRUE(context.is_in("^"));
+}
