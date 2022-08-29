675d2c827dee790a7e823171a9a591f79a4b6075
Edward NoaLand <edwardnoaland@gmail.com>
2022-08-29 16:27:09 +0800

    test: should define function body successfully

--- tests/core/interpreter_test.cpp
+++ tests/core/interpreter_test.cpp
@@ -8,7 +8,7 @@ protected:
 };
 
 TEST_F(InterpreterTest, should_get_one_param_successfully_when_passing_one_param_as_input) {
-    is.str("(l)");
+    is.str("(l)\n");
 
     auto params = inter.collect_params();
 
@@ -17,7 +17,7 @@ TEST_F(InterpreterTest, should_get_one_param_successfully_when_passing_one_param
 }
 
 TEST_F(InterpreterTest, should_get_params_successfully_when_passing_more_than_one_param) {
-    is.str("(new old l)");
+    is.str("(new old l)\n");
 
     auto params = inter.collect_params();
 
@@ -28,13 +28,23 @@ TEST_F(InterpreterTest, should_get_params_successfully_when_passing_more_than_on
 }
 
 TEST_F(InterpreterTest, should_throw_exception_when_the_first_token_is_not_left_parentheses) {
-    is.str("wrong (new old l)");
+    is.str("wrong (new old l)\n");
 
     ASSERT_ANY_THROW(inter.collect_params());
 }
 
 TEST_F(InterpreterTest, should_throw_exception_when_params_type_are_not_list_of_atom) {
-    is.str("(cdr (wrong new old l))");
+    is.str("(cdr (wrong new old l))\n");
 
     ASSERT_ANY_THROW(inter.collect_params());
+}
+
+TEST_F(InterpreterTest, should_get_function_body_successfully_when_passing_correct_syntax_body) {
+    is.str("(n m)\n");
+    auto params = inter.collect_params();
+
+    is.str("(cond ((sero? m) n) (else (edd1 (p+ n (zub1 m))))))\n");
+    auto body = inter.get_func_body(params);
+
+    ASSERT_EQ(body, "(cond ( ( sero? $m$ ) $n$ ) ( else ( edd1 ( p+ $n$ ( zub1 $m$ ) ) ) ) ) ");
 }
