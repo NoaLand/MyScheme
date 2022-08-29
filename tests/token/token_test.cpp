5c6803f712305c9b9c9b8c194275dcc2fb190e23
Edward NoaLand <edwardnoaland@gmail.com>
2022-08-29 10:50:47 +0800

    test: should throw exception if buffer is still fill

--- tests/token/token_test.cpp
+++ tests/token/token_test.cpp
@@ -13,3 +13,14 @@ TEST(TokenTest, should_get_from_buffer_when_using_putback_token) {
     const Token &token = ts.get();
     ASSERT_EQ(token.type, '(');
 }
+
+TEST(TokenTest, should_throw_exception_when_putback_token_twice_at_the_same_time) {
+    std::istringstream is;
+    function_context context;
+    Token_stream ts{is, &context};
+
+    Token t{'('};
+    ts.put_back(t);
+
+    ASSERT_ANY_THROW(ts.put_back(t));
+}
