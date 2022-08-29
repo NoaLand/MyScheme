5fe277e1c2d83dbace5537792c0d4d69c50f8f6f
Edward NoaLand <edwardnoaland@gmail.com>
2022-08-29 16:06:42 +0800

    test: should get params successfully with collect param

--- tests/core/interpreter_test.cpp
+++ tests/core/interpreter_test.cpp
@@ -15,3 +15,14 @@ TEST_F(InterpreterTest, should_get_one_param_successfully_when_passing_one_param
     ASSERT_EQ(params->size_of(), 1);
     ASSERT_EQ(params->get(0)->get_value(), "l");
 }
+
+TEST_F(InterpreterTest, should_get_params_successfully_when_passing_more_than_one_param) {
+    is.str("(new old l)");
+
+    auto params = inter.collect_params();
+
+    ASSERT_EQ(params->size_of(), 3);
+    ASSERT_EQ(params->get(0)->get_value(), "new");
+    ASSERT_EQ(params->get(1)->get_value(), "old");
+    ASSERT_EQ(params->get(2)->get_value(), "l");
+}
