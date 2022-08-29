e3b8aa1612762198709d467bc974469507985256
Edward NoaLand <edwardnoaland@gmail.com>
2022-08-29 14:39:17 +0800

    refactor: add new base test class for fixture and param test

--- tests/token/token_test.cpp
+++ tests/token/token_test.cpp
@@ -3,20 +3,14 @@
 #include "preload_libs/preload_libs.h"
 #include "token/token.h"
 
-class TokenTest: public testing::TestWithParam<std::string> {
+class BaseTokenTest: public testing::Test {
 protected:
     std::istringstream is;
     function_context context;
     Token_stream ts{is, &context};
 };
 
-INSTANTIATE_TEST_SUITE_P(PreloadFunc,
-                         TokenTest,
-                         testing::Values("quote", "car", "cdr", "cons", "null?", "addtup", "atom?", "eq?", "number?",
-                                         "cond", "or?","and?", "zero?", "add1", "sub1", "lat?", "length", "member?",
-                                         "rember", "pick", "rempick", "+", "-", "*", "/", ">", "<", "=", "^"));
-
-TEST_F(TokenTest, should_get_from_buffer_when_using_putback_token) {
+TEST_F(BaseTokenTest, should_get_from_buffer_when_using_putback_token) {
     Token t{'('};
     ts.put_back(t);
 
@@ -25,14 +19,14 @@ TEST_F(TokenTest, should_get_from_buffer_when_using_putback_token) {
     ASSERT_EQ(token.type, '(');
 }
 
-TEST_F(TokenTest, should_throw_exception_when_putback_token_twice_at_the_same_time) {
+TEST_F(BaseTokenTest, should_throw_exception_when_putback_token_twice_at_the_same_time) {
     Token t{'('};
     ts.put_back(t);
 
     ASSERT_ANY_THROW(ts.put_back(t));
 }
 
-TEST_F(TokenTest, should_return_token_with_left_parentheses_when_it_is_in_istream) {
+TEST_F(BaseTokenTest, should_return_token_with_left_parentheses_when_it_is_in_istream) {
     is.str("(\n");
 
     const Token &token = ts.get();
@@ -40,7 +34,7 @@ TEST_F(TokenTest, should_return_token_with_left_parentheses_when_it_is_in_istrea
     ASSERT_EQ(token.type, '(');
 }
 
-TEST_F(TokenTest, should_return_token_with_N_type_when_getting_an_integer_123) {
+TEST_F(BaseTokenTest, should_return_token_with_N_type_when_getting_an_integer_123) {
     is.str("123\n");
     Token_stream ts{is, &context};
 
@@ -50,7 +44,7 @@ TEST_F(TokenTest, should_return_token_with_N_type_when_getting_an_integer_123) {
     ASSERT_EQ(token.integer_value, 123);
 }
 
-TEST_F(TokenTest, should_return_A_type_when_getting_input_is_123ABC){
+TEST_F(BaseTokenTest, should_return_A_type_when_getting_input_is_123ABC){
     is.str("123ABC\n");
 
     const Token &token = ts.get();
@@ -59,7 +53,7 @@ TEST_F(TokenTest, should_return_A_type_when_getting_input_is_123ABC){
     ASSERT_EQ(token.value, "123ABC");
 }
 
-TEST_F(TokenTest, should_return_D_type_when_getting_input_is_define) {
+TEST_F(BaseTokenTest, should_return_D_type_when_getting_input_is_define) {
     is.str("define\n");
 
     const Token &token = ts.get();
@@ -68,7 +62,7 @@ TEST_F(TokenTest, should_return_D_type_when_getting_input_is_define) {
     ASSERT_EQ(token.value, "");
 }
 
-TEST_F(TokenTest, should_return_L_type_when_getting_input_is_lambda) {
+TEST_F(BaseTokenTest, should_return_L_type_when_getting_input_is_lambda) {
     is.str("lambda\n");
 
     const Token &token = ts.get();
@@ -77,7 +71,16 @@ TEST_F(TokenTest, should_return_L_type_when_getting_input_is_lambda) {
     ASSERT_EQ(token.value, "");
 }
 
-TEST_P(TokenTest, should_return_F_type_when_token_is_stored) {
+class PrimitiveAndFuncTokenTest: public BaseTokenTest,
+                                 public testing::WithParamInterface<std::string> {};
+
+INSTANTIATE_TEST_SUITE_P(PreloadFunc,
+                         PrimitiveAndFuncTokenTest,
+                         testing::Values("quote", "car", "cdr", "cons", "null?", "addtup", "atom?", "eq?", "number?",
+                                         "cond", "or?","and?", "zero?", "add1", "sub1", "lat?", "length", "member?",
+                                         "rember", "pick", "rempick", "+", "-", "*", "/", ">", "<", "=", "^"));
+
+TEST_P(PrimitiveAndFuncTokenTest, should_return_F_type_when_token_is_stored) {
     preload_libs(context);
     std::string func_name = GetParam();
     is.str(func_name + "\n");
