f74f13b493a82d172b14dcc02bff9ca82b82643a
Edward NoaLand <edwardnoaland@gmail.com>
2022-08-29 09:37:23 +0800

    test: should return true when input is an atom

--- tests/function/s_expression_family_test.cpp
+++ tests/function/s_expression_family_test.cpp
@@ -10,3 +10,13 @@ TEST(IsAtomTest, should_get_basic_info_when_successfully_create_is_atom_expressi
     ASSERT_EQ(f->return_type(), "bool");
     ASSERT_EQ(f->family(), "s_expression");
 }
+
+TEST(IsAtomTest, should_return_true_when_input_is_an_atom) {
+    auto a = new atom{"a"};
+
+    auto f = new is_atom{a};
+    auto res = f->execute();
+
+    ASSERT_EQ(res->get_indicator(), "bool");
+    ASSERT_TRUE(res->val());
+}
