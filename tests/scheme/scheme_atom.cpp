3c439c5c4c18725c98948c5b7e472ae7a76b3989
Edward NoaLand <edwardnoaland@gmail.com>
2022-08-30 11:14:31 +0800

    refactor: use UseCase in baseTest

--- tests/scheme/scheme_atom.cpp
+++ tests/scheme/scheme_atom.cpp
@@ -7,13 +7,18 @@ protected:
 
 INSTANTIATE_TEST_SUITE_P(AtomGroup,
                          SchemeAtomGroupTest,
-                         testing::Values("atom", "turkey", "u", "*abc$"));
+                         testing::Values(
+                                 UseCase("atom", "atom: atom"),
+                                 UseCase("turkey", "atom: turkey"),
+                                 UseCase("u", "atom: u"),
+                                 UseCase("*abc$", "atom: *abc$")
+                         ));
 
 TEST_P(SchemeAtomGroupTest, should_return_F_type_when_token_is_stored) {
-    std::string an_atom = GetParam() + "\n";
+    std::string an_atom = GetParam().input;
     is.str(an_atom);
 
     inter.scheme();
 
-    ASSERT_THAT(os.str(), HasSubstr("atom: " + an_atom));
+    ASSERT_THAT(os.str(), HasSubstr(GetParam().expected_output));
 }
--- tests/scheme/scheme_use_case.h
+++ tests/scheme/scheme_use_case.h
@@ -3,10 +3,19 @@
 
 #include <base_test.h>
 
+#include <utility>
+
 #include "core/interpreter.h"
 
+class UseCase {
+public:
+    UseCase(std::string i, std::string e): input(std::move(i) + "\n"), expected_output(std::move(e)) {};
+    std::string input;
+    std::string expected_output;
+};
+
 class SchemeUseCaseBaseTest: public BaseTest,
-                             public testing::WithParamInterface<std::string> {
+                             public testing::WithParamInterface<UseCase> {
 protected:
     interpreter inter{context, ts};
 };
