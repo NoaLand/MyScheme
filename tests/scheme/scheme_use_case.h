99b03adb729bc77751d700ae9d059b040fa2561a
Edward NoaLand <edwardnoaland@gmail.com>
2022-08-30 15:46:22 +0800

    TODO: need to deal with list template class implementation

--- tests/scheme/scheme_use_case.h
+++ tests/scheme/scheme_use_case.h
@@ -16,6 +16,7 @@ public:
         input(std::move(i) + "\n"),
         handle_throw(exception) {
 
+        // TODO: ugly code, this is solve list<X> is list and list<integer> is tuple, this condition indicates that list implementation needs to be improved
         if(!std::is_abstract_v<T>) {
             if(std::is_same_v<T, list<atom>>
                 || std::is_same_v<T, list<boolean>>
