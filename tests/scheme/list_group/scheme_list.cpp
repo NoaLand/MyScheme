cded3646c79e7f7fc27bafea116db1c37f45e42f
Edward NoaLand <edwardnoaland@gmail.com>
2022-08-30 11:22:28 +0800

    feat: add list use case

---
+++ tests/scheme/list_group/scheme_list.cpp
@@ -0,0 +1,22 @@
+#include "scheme/scheme_use_case.h"
+
+class SchemeListGroupTest: public SchemeUseCaseBaseTest {
+};
+
+INSTANTIATE_TEST_SUITE_P(AtomGroup,
+                         SchemeListGroupTest,
+                         testing::Values(
+                                 UseCase("(atom)", "list: ( atom )"),
+                                 UseCase("(atom turkey or)", "list: ( atom turkey or )"),
+                                 UseCase("((atom turkey) or)", "list: ( ( atom turkey ) or )"),
+                                 UseCase("((x y) z)", "list: ( ( x y ) z )")
+                         ));
+
+TEST_P(SchemeListGroupTest, should_return_expected_list_from_scheme_interpreter) {
+    std::string an_atom = GetParam().input;
+    is.str(an_atom);
+
+    inter.scheme();
+
+    ASSERT_THAT(os.str(), HasSubstr(GetParam().expected_output));
+}
