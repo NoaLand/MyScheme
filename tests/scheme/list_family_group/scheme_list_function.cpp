0c1b6d3a588e3b5520ee12fc1943656c887050a0
Edward NoaLand <edwardnoaland@gmail.com>
2022-08-30 13:46:40 +0800

    feat: add cdr use cases

--- tests/scheme/list_family_group/scheme_list_function.cpp
+++ tests/scheme/list_family_group/scheme_list_function.cpp
@@ -17,3 +17,20 @@ TEST_P(CarFunctionGroupTest, should_return_expected_car_res_from_scheme_interpre
 
     scheme(use_case);
 }
+
+class CdrFunctionGroupTest: public SchemeUseCaseBaseTest {
+};
+
+INSTANTIATE_TEST_SUITE_P(CdrGroup,
+                         CdrFunctionGroupTest,
+                         testing::Values(
+                                 UseCase("(cdr ((a b c) x y z))", "list: ( x y z )"),
+                                 UseCase("(cdr (hamburger))", "tuple: ( )"),
+                                 UseCase("(cdr ((x) t r))", "list: ( t r )")
+                         ));
+
+TEST_P(CdrFunctionGroupTest, should_return_expected_cdr_res_from_scheme_interpreter) {
+    UseCase use_case = GetParam();
+
+    scheme(use_case);
+}
