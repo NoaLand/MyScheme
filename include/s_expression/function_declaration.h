6ef53e18bcb8784df6d49b84787dcd736946152b
Edward NoaLand <edwardnoaland@gmail.com>
2022-08-30 20:55:34 +0800

    refactor: add std::vector<Token> as function definition

--- include/s_expression/function_declaration.h
+++ include/s_expression/function_declaration.h
@@ -10,10 +10,12 @@ class function_declaration: public s_expression {
 public:
     function_declaration() = default;
     function_declaration(std::string n, list<param>* p, std::string b): name(std::move(n)), params(p), serialized_body(std::move(b)) {}
-    function_declaration(std::string n, list<param>* p, std::vector<Token> b): name(std::move(n)), params(p), serialized_body(serialize_token_list(b)) {}
+
+    function_declaration(std::string n, list<param>* p, std::vector<Token> b): name(std::move(n)), params(p), body(b), serialized_body(serialize_token_list(b)) {}
+    
     std::string get_name() { return name; }
     list<param>* get_params() { return params; }
-    std::string get_body() { return serialized_body; }
+    std::string get_body() { return serialize_token_list(body); }
     std::string get_value() override;
     std::string get_indicator() override { return indicator; }
     void print(std::ostream& os) override;
@@ -22,6 +24,7 @@ private:
     std::string name;
     list<param>* params{};
     std::string serialized_body;
+    std::vector<Token> body;
 };
 
 #endif //MYSCHEME_FUNCTION_DECLARATION_H
