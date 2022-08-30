78be8e389e36dfa38c4aa0e4e05ff8b497490589
Edward NoaLand <edwardnoaland@gmail.com>
2022-08-30 11:28:14 +0800

    feat: add use cases to list

--- tests/scheme/list_group/scheme_list.cpp
+++ tests/scheme/list_group/scheme_list.cpp
@@ -12,7 +12,8 @@ INSTANTIATE_TEST_SUITE_P(AtomGroup,
                                  UseCase("((x y) z)", "list: ( ( x y ) z )"),
                                  UseCase("(how are you doing so far)", "list: ( how are you doing so far )"),
                                  UseCase("(((how) are) ((you) (doing so)) far)", "list: ( ( ( how ) are ) ( ( you ) ( doing so ) ) far )"),
-                                 UseCase("()", "tuple: ( )")
+                                 UseCase("()", "tuple: ( )"),
+                                 UseCase("(() () () ())", "list: ( ( ) ( ) ( ) ( ) )")
                          ));
 
 TEST_P(SchemeListGroupTest, should_return_expected_list_from_scheme_interpreter) {
