ce35093d68fdd2ea2982177729c2595a39333605
Edward NoaLand <edwardnoaland@gmail.com>
2022-08-31 15:54:37 +0800

    format: format use case in firsts

--- tests/scheme/customized_function_group/customized_function.cpp
+++ tests/scheme/customized_function_group/customized_function.cpp
@@ -68,7 +68,10 @@ class FirstGroupTest: public SchemeUseCaseBaseTest {
 INSTANTIATE_TEST_SUITE_P(FirstGroup,
                          FirstGroupTest,
                          testing::Values(
-                                 UseCase<list<atom>>("(firsts ((apple peach pumpkin) (plum pear cherry) (grape raisin pea) (bean carrot eggplant)))", "( apple plum grape bean )")
+                                 UseCase<list<atom>>("(firsts ((apple peach pumpkin)"
+                                                                "(plum pear cherry)"
+                                                                "(grape raisin pea)"
+                                                                "(bean carrot eggplant)))", "( apple plum grape bean )")
                          ));
 
 TEST_P(FirstGroupTest, should_return_expected_first_res_from_scheme_interpreter) {
