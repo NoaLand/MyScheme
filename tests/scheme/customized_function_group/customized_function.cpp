5574d78f474e13e704105b477fa8f649cb40a56a
Edward NoaLand <edwardnoaland@gmail.com>
2022-08-31 15:24:07 +0800

    feat: add more use case for lat?

--- tests/scheme/customized_function_group/customized_function.cpp
+++ tests/scheme/customized_function_group/customized_function.cpp
@@ -6,7 +6,13 @@ class IsLatListGroupTest: public SchemeUseCaseBaseTest {
 INSTANTIATE_TEST_SUITE_P(IsLatGroup,
                          IsLatListGroupTest,
                          testing::Values(
-                                 UseCase<boolean>("(lat? (Jack Sprat could eat no chicken fat))", "#t")
+                                 UseCase<boolean>("(lat? (Jack Sprat could eat no chicken fat))", "#t"),
+                                 UseCase<boolean>("(lat? ((Jack) Sprat could eat no chicken fat))", "#f"),
+                                 UseCase<boolean>("(lat? (Jack (Sprat could) eat no chicken fat))", "#f"),
+                                 UseCase<boolean>("(lat? ())", "#t"),
+                                 UseCase<boolean>("(lat? (bacon and eggs))", "#t"),
+                                 UseCase<boolean>("(lat? (bacon (and eggs)))", "#f"),
+                                 UseCase<boolean>("(lat? (((tomato sauce)) ((bean) sauce) (and ((flying)) sauce)))", "#f")
                          ));
 
 TEST_P(IsLatListGroupTest, should_return_expected_list_from_scheme_interpreter) {
