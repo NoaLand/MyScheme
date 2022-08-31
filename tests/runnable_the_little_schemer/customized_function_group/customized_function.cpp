021520d3535d27f901bebf96f11c14c3ecf524ab
Edward NoaLand <edwardnoaland@gmail.com>
2022-08-31 18:20:53 +0800

    feat: add member* use case

--- tests/runnable_the_little_schemer/customized_function_group/customized_function.cpp
+++ tests/runnable_the_little_schemer/customized_function_group/customized_function.cpp
@@ -375,3 +375,19 @@ TEST_P(InsertLStarGroupTest, should_return_expected_insertL_res_from_scheme_inte
     UseCase use_case = GetParam();
     scheme(use_case);
 }
+
+class IsMemberStarGroupTest: public SchemeUseCaseBaseTest {
+};
+
+INSTANTIATE_TEST_SUITE_P(IsMemberStarGroup,
+                         IsMemberStarGroupTest,
+                         testing::Values(
+                                 UseCase<boolean>("(member* chips ((potato) (chips ((with) fish) (chips))))", "#t")
+                         ));
+
+TEST_P(IsMemberStarGroupTest, should_return_expected_member_boolean_res_from_scheme_interpreter) {
+    function_define("member*", "(define member* (lambda (a l) (cond ((null? l) #f) ((atom? (car l)) (cond ((eq? a (car l)) #t) (else (or? #f (member* a (cdr l)))))) (else (or? (member* a (car l)) (member* a (cdr l)))))))");
+
+    UseCase use_case = GetParam();
+    scheme(use_case);
+}
