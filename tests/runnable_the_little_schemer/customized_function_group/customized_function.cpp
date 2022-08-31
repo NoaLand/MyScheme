7e6334cc49ece45b4ccd5da69857fb7c225952b1
Edward NoaLand <edwardnoaland@gmail.com>
2022-08-31 18:11:09 +0800

    feat: add insertR* use case

--- tests/runnable_the_little_schemer/customized_function_group/customized_function.cpp
+++ tests/runnable_the_little_schemer/customized_function_group/customized_function.cpp
@@ -303,8 +303,25 @@ INSTANTIATE_TEST_SUITE_P(RememberStarGroup,
                                  UseCase<list<s_expression>>("(rember* sauce (((tomato sauce)) ((bean) sauce) (and ((flying)) sauce)))", "( ( ( tomato ) ) ( ( bean ) ) ( and ( ( flying ) ) ) )")
                          ));
 
-TEST_P(RememberStarGroupTest, should_return_expected_rember_list_from_scheme_interpreter) {
+TEST_P(RememberStarGroupTest, should_return_expected_remberstar_list_from_scheme_interpreter) {
     function_define("rember*", "(define rember* (lambda (a l) (cond ((null? l) ()) ((atom? (car l)) (cond ((eq? a (car l)) (rember* a (cdr l))) (else (cons (car l) (rember* a (cdr l)))))) (else (cons (rember* a (car l)) (rember* a (cdr l)))))))");
+
+    UseCase use_case = GetParam();
+    scheme(use_case);
+}
+
+class InsertRStarGroupTest: public SchemeUseCaseBaseTest {
+};
+
+INSTANTIATE_TEST_SUITE_P(InsertRGroup,
+                         InsertRStarGroupTest,
+                         testing::Values(
+                                 UseCase<list<atom>>("(insertR* roast chuck ((how much (wood)) could ((a (wood) chuck)) (((chuck))) (if (a) ((wood chuck))) could chuck wood))",
+                                                   "( ( how much ( wood ) ) could ( ( a ( wood ) chuck roast ) ) ( ( ( chuck roast ) ) ) ( if ( a ) ( ( wood chuck roast ) ) ) could chuck roast wood )")
+                         ));
+
+TEST_P(InsertRStarGroupTest, should_return_expected_insertRstar_res_from_scheme_interpreter) {
+    function_define("insertR*", "(define insertR* (lambda (new old l) (cond ((null? l) ()) ((atom? (car l)) (cond ((eq? (car l) old) (cons old (cons new (insertR* new old (cdr l))))) (else (cons (car l) (insertR* new old (cdr l)))))) (else (cons (insertR* new old (car l)) (insertR* new old (cdr l)))))))");
 
     UseCase use_case = GetParam();
     scheme(use_case);
