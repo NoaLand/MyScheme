81d823cce3bc3b1404812754704fd7ecd08482f9
Edward NoaLand <edwardnoaland@gmail.com>
2024-01-12 10:02:24 +0800

    refactor: remove all new operator

--- tests/function/list_family_test.cpp
+++ tests/function/list_family_test.cpp
@@ -3,68 +3,68 @@
 #include "function/list_family.h"
 
 TEST(CarTest, should_get_correct_basic_info_after_successfully_init_car_expression) {
-    auto l = new list<atom>{};
-    auto a1 = new atom{"a1"};
-    auto a2 = new atom{"a2"};
-    auto a3 = new atom{"a3"};
-    l->push_back(a1);
-    l->push_back(a2);
-    l->push_back(a3);
+    auto l = list<atom>{};
+    auto a1 = atom{"a1"};
+    auto a2 = atom{"a2"};
+    auto a3 = atom{"a3"};
+    l.push_back(&a1);
+    l.push_back(&a2);
+    l.push_back(&a3);
 
-    auto f = new car{l};
-    ASSERT_EQ(f->name(), "car");
-    ASSERT_EQ(f->return_type(), "s_expression");
-    ASSERT_EQ(f->family(), "list");
+    auto f = car{&l};
+    ASSERT_EQ(f.name(), "car");
+    ASSERT_EQ(f.return_type(), "s_expression");
+    ASSERT_EQ(f.family(), "list");
 }
 
 TEST(CarTest, should_return_a1_when_car_get_a1_a2_a3_list) {
-    auto l = new list<atom>{};
-    auto a1 = new atom{"a1"};
-    auto a2 = new atom{"a2"};
-    auto a3 = new atom{"a3"};
-    l->push_back(a1);
-    l->push_back(a2);
-    l->push_back(a3);
+    auto l = list<atom>{};
+    auto a1 = atom{"a1"};
+    auto a2 = atom{"a2"};
+    auto a3 = atom{"a3"};
+    l.push_back(&a1);
+    l.push_back(&a2);
+    l.push_back(&a3);
 
-    auto f = new car{l};
-    auto res = f->execute();
+    auto f = car{&l};
+    auto res = f.execute();
 
     ASSERT_EQ(res->get_indicator(), "atom");
     ASSERT_EQ(res->get_value(), "a1");
 }
 
 TEST(CarTest, should_return_1_when_car_get_1_2_3_tuple) {
-    auto l = new list<integer>{};
-    auto i1 = new integer{1};
-    auto i2 = new integer{2};
-    auto i3 = new integer{3};
-    l->push_back(i1);
-    l->push_back(i2);
-    l->push_back(i3);
+    auto l = list<integer>{};
+    auto i1 = integer{1};
+    auto i2 = integer{2};
+    auto i3 = integer{3};
+    l.push_back(&i1);
+    l.push_back(&i2);
+    l.push_back(&i3);
 
-    auto f = new car{l};
-    auto res = f->execute();
+    auto f = car{&l};
+    auto res = f.execute();
 
     ASSERT_EQ(res->get_indicator(), "integer");
     ASSERT_EQ(dynamic_cast<integer*>(res)->val(), 1);
 }
 
 TEST(CarTest, should_return_a1_a2_list_when_car_get_a1_a2_list_a3_a4) {
-    auto l1 = new list<atom>{};
-    auto a1 = new atom{"a1"};
-    auto a2 = new atom{"a2"};
-    l1->push_back(a1);
-    l1->push_back(a2);
+    auto l1 = list<atom>{};
+    auto a1 = atom{"a1"};
+    auto a2 = atom{"a2"};
+    l1.push_back(&a1);
+    l1.push_back(&a2);
 
-    auto l2 = new list<s_expression>{};
-    auto a3 = new atom{"a3"};
-    auto a4 = new atom{"a4"};
-    l2->push_back(l1);
-    l2->push_back(a3);
-    l2->push_back(a4);
+    auto l2 = list<s_expression>{};
+    auto a3 = atom{"a3"};
+    auto a4 = atom{"a4"};
+    l2.push_back(&l1);
+    l2.push_back(&a3);
+    l2.push_back(&a4);
 
-    auto f = new car{l2};
-    auto res = f->execute();
+    auto f = car{&l2};
+    auto res = f.execute();
 
     ASSERT_EQ(res->get_indicator(), "list");
     ASSERT_EQ(dynamic_cast<list<atom>*>(res)->size_of(), 2);
@@ -73,42 +73,42 @@ TEST(CarTest, should_return_a1_a2_list_when_car_get_a1_a2_list_a3_a4) {
 }
 
 TEST(CarTest, should_throw_exception_when_create_car_primitive_if_passing_s_expression_is_not_a_list) {
-    auto err_input = new atom{"err_input"};
-    ASSERT_ANY_THROW(new car{err_input});
+    auto err_input = atom{"err_input"};
+    ASSERT_ANY_THROW(car{&err_input});
 }
 
 TEST(CarTest, should_throw_exception_when_call_car_primitive_execute_with_empty_list) {
-    auto empty_list = new list<atom>{};
-    auto f = new car{empty_list};
-    ASSERT_ANY_THROW(f->execute());
+    auto empty_list = list<atom>{};
+    auto f = car{&empty_list};
+    ASSERT_ANY_THROW(f.execute());
 }
 
 TEST(CdrTest, should_get_correct_basic_info_after_successfully_init_cdr_expression) {
-    auto l = new list<atom>{};
-    auto a1 = new atom{"a1"};
-    auto a2 = new atom{"a2"};
-    auto a3 = new atom{"a3"};
-    l->push_back(a1);
-    l->push_back(a2);
-    l->push_back(a3);
+    auto l = list<atom>{};
+    auto a1 = atom{"a1"};
+    auto a2 = atom{"a2"};
+    auto a3 = atom{"a3"};
+    l.push_back(&a1);
+    l.push_back(&a2);
+    l.push_back(&a3);
 
-    auto f = new cdr{l};
-    ASSERT_EQ(f->name(), "cdr");
-    ASSERT_EQ(f->return_type(), "list");
-    ASSERT_EQ(f->family(), "list");
+    auto f = cdr{&l};
+    ASSERT_EQ(f.name(), "cdr");
+    ASSERT_EQ(f.return_type(), "list");
+    ASSERT_EQ(f.family(), "list");
 }
 
 TEST(CdrTest, should_return_a2_a3_when_cdr_get_a1_a2_a3_list) {
-    auto l = new list<atom>{};
-    auto a1 = new atom{"a1"};
-    auto a2 = new atom{"a2"};
-    auto a3 = new atom{"a3"};
-    l->push_back(a1);
-    l->push_back(a2);
-    l->push_back(a3);
+    auto l = list<atom>{};
+    auto a1 = atom{"a1"};
+    auto a2 = atom{"a2"};
+    auto a3 = atom{"a3"};
+    l.push_back(&a1);
+    l.push_back(&a2);
+    l.push_back(&a3);
 
-    auto f = new cdr{l};
-    auto res = f->execute();
+    auto f = cdr{&l};
+    auto res = f.execute();
 
     ASSERT_EQ(res->get_indicator(), "list");
     ASSERT_EQ(reinterpret_cast<list<atom>*>(res)->size_of(), 2);
@@ -117,16 +117,16 @@ TEST(CdrTest, should_return_a2_a3_when_cdr_get_a1_a2_a3_list) {
 }
 
 TEST(CdrTest, should_return_2_3_when_cdr_get_1_2_3_tuple) {
-    auto l = new list<integer>{};
-    auto i1 = new integer{1};
-    auto i2 = new integer{2};
-    auto i3 = new integer{3};
-    l->push_back(i1);
-    l->push_back(i2);
-    l->push_back(i3);
+    auto l = list<integer>{};
+    auto i1 = integer{1};
+    auto i2 = integer{2};
+    auto i3 = integer{3};
+    l.push_back(&i1);
+    l.push_back(&i2);
+    l.push_back(&i3);
 
-    auto f = new cdr{l};
-    auto res = f->execute();
+    auto f = cdr{&l};
+    auto res = f.execute();
 
     ASSERT_EQ(res->get_indicator(), "tuple");
     ASSERT_EQ(reinterpret_cast<list<integer>*>(res)->size_of(), 2);
@@ -135,22 +135,22 @@ TEST(CdrTest, should_return_2_3_when_cdr_get_1_2_3_tuple) {
 }
 
 TEST(CdrTest, should_return_a2_a3_list_and_a4_when_cdr_get_a1_and_a2_a3_list_a4) {
-    auto a1 = new atom{"a1"};
-    auto l1 = new list<atom>{};
-    auto a2 = new atom{"a2"};
-    auto a3 = new atom{"a3"};
-    l1->push_back(a2);
-    l1->push_back(a3);
+    auto a1 = atom{"a1"};
+    auto l1 = list<atom>{};
+    auto a2 = atom{"a2"};
+    auto a3 = atom{"a3"};
+    l1.push_back(&a2);
+    l1.push_back(&a3);
 
-    auto l2 = new list<s_expression>{};
-    auto a4 = new atom{"a4"};
+    auto l2 = list<s_expression>{};
+    auto a4 = atom{"a4"};
 
-    l2->push_back(a1);
-    l2->push_back(l1);
-    l2->push_back(a4);
+    l2.push_back(&a1);
+    l2.push_back(&l1);
+    l2.push_back(&a4);
 
-    auto f = new cdr{l2};
-    auto res = f->execute();
+    auto f = cdr{&l2};
+    auto res = f.execute();
 
     ASSERT_EQ(res->get_indicator(), "list");
     ASSERT_EQ(dynamic_cast<list<s_expression>*>(res)->size_of(), 2);
@@ -166,38 +166,38 @@ TEST(CdrTest, should_return_a2_a3_list_and_a4_when_cdr_get_a1_and_a2_a3_list_a4)
 }
 
 TEST(CdrTest, should_throw_exception_when_create_cdr_primitive_if_passing_s_expression_is_not_a_list) {
-    auto err_input = new atom{"err_input"};
-    ASSERT_ANY_THROW(new cdr{err_input});
+    auto err_input = atom{"err_input"};
+    ASSERT_ANY_THROW(cdr{&err_input});
 }
 
 TEST(CdrTest, should_throw_exception_when_call_cdr_primitive_execute_with_empty_list) {
-    auto empty_list = new list<atom>{};
-    auto f = new cdr{empty_list};
-    ASSERT_ANY_THROW(f->execute());
+    auto empty_list = list<atom>{};
+    auto f = cdr{&empty_list};
+    ASSERT_ANY_THROW(f.execute());
 }
 
 TEST(ConsTest, should_get_correct_basic_info_after_successfully_init_cons_expression) {
-    auto a1 = new atom{"a1"};
+    auto a1 = atom{"a1"};
 
-    auto l = new list<atom>{};
-    auto a2 = new atom{"a2"};
-    l->push_back(a2);
+    auto l = list<atom>{};
+    auto a2 = atom{"a2"};
+    l.push_back(&a2);
 
-    auto f = new cons{a1, l};
-    ASSERT_EQ(f->name(), "cons");
-    ASSERT_EQ(f->return_type(), "list");
-    ASSERT_EQ(f->family(), "list");
+    auto f = cons{&a1, &l};
+    ASSERT_EQ(f.name(), "cons");
+    ASSERT_EQ(f.return_type(), "list");
+    ASSERT_EQ(f.family(), "list");
 }
 
 TEST(ConsTest, should_return_a1_a2_list_when_cons_a1_to_a2_list) {
-    auto a1 = new atom{"a1"};
+    auto a1 = atom{"a1"};
 
-    auto l = new list<atom>{};
-    auto a2 = new atom{"a2"};
-    l->push_back(a2);
+    auto l = list<atom>{};
+    auto a2 = atom{"a2"};
+    l.push_back(&a2);
 
-    auto f = new cons{a1, l};
-    auto res = f->execute();
+    auto f = cons{&a1, &l};
+    auto res = f.execute();
     ASSERT_EQ(res->get_indicator(), "list");
 
     auto res_to_list = reinterpret_cast<list<atom>*>(res);
@@ -207,14 +207,14 @@ TEST(ConsTest, should_return_a1_a2_list_when_cons_a1_to_a2_list) {
 }
 
 TEST(ConsTest, should_return_1_2_tuple_when_cons_1_to_2_tuple) {
-    auto i1 = new integer{1};
+    auto i1 = integer{1};
 
-    auto l = new list<integer>{};
-    auto i2 = new integer{2};
-    l->push_back(i2);
+    auto l = list<integer>{};
+    auto i2 = integer{2};
+    l.push_back(&i2);
 
-    auto f = new cons{i1, l};
-    auto res = f->execute();
+    auto f = cons{&i1, &l};
+    auto res = f.execute();
     ASSERT_EQ(res->get_indicator(), "tuple");
 
     auto res_to_list = reinterpret_cast<list<integer>*>(res);
@@ -224,18 +224,18 @@ TEST(ConsTest, should_return_1_2_tuple_when_cons_1_to_2_tuple) {
 }
 
 TEST(ConsTest, should_return_a1_a2_list_in_a3_list_when_cons_a1_a2_list_to_a3_list) {
-    auto l1 = new list<atom>{};
-    auto a1 = new atom{"a1"};
-    auto a2 = new atom{"a2"};
-    l1->push_back(a1);
-    l1->push_back(a2);
+    auto l1 = list<atom>{};
+    auto a1 = atom{"a1"};
+    auto a2 = atom{"a2"};
+    l1.push_back(&a1);
+    l1.push_back(&a2);
 
-    auto l2 = new list<atom>{};
-    auto a3 = new atom{"a3"};
-    l2->push_back(a3);
+    auto l2 = list<atom>{};
+    auto a3 = atom{"a3"};
+    l2.push_back(&a3);
 
-    auto f = new cons{l1, l2};
-    auto res = f->execute();
+    auto f = cons{&l1, &l2};
+    auto res = f.execute();
     ASSERT_EQ(res->get_indicator(), "list");
 
     auto res_to_list = reinterpret_cast<list<s_expression>*>(res);
@@ -250,104 +250,104 @@ TEST(ConsTest, should_return_a1_a2_list_in_a3_list_when_cons_a1_a2_list_to_a3_li
 }
 
 TEST(ConsTest, should_throw_exception_when_cons_right_param_is_not_a_list) {
-    auto a1 = new atom{"a1"};
-    auto a2 = new atom{"a2"};
-    ASSERT_ANY_THROW(new cons(a1, a2));
+    auto a1 = atom{"a1"};
+    auto a2 = atom{"a2"};
+    ASSERT_ANY_THROW(cons(&a1, &a2));
 }
 
 TEST(IsNullTest, should_get_correct_basic_info_after_successfully_init_is_null_expression) {
-    auto l = new list<atom>{};
-    auto f = new is_null{l};
+    auto l = list<atom>{};
+    auto f = is_null{&l};
 
-    ASSERT_EQ(f->name(), "null?");
-    ASSERT_EQ(f->return_type(), "bool");
-    ASSERT_EQ(f->family(), "list");
+    ASSERT_EQ(f.name(), "null?");
+    ASSERT_EQ(f.return_type(), "bool");
+    ASSERT_EQ(f.family(), "list");
 }
 
 TEST(IsNullTest, should_return_true_when_is_null_getting_an_empty_list) {
-    auto l = new list<atom>{};
+    auto l = list<atom>{};
 
-    auto f = new is_null{l};
-    auto res = f->execute();
+    auto f = is_null{&l};
+    auto res = f.execute();
 
     ASSERT_EQ(res->get_indicator(), "bool");
     ASSERT_TRUE(dynamic_cast<boolean*>(res)->val());
 }
 
 TEST(IsNullTest, should_return_false_when_is_null_getting_non_empty_list) {
-    auto l = new list<atom>{};
-    auto a1 = new atom{"a1"};
-    l->push_back(a1);
+    auto l = list<atom>{};
+    auto a1 = atom{"a1"};
+    l.push_back(&a1);
 
-    auto f = new is_null{l};
-    auto res = f->execute();
+    auto f = is_null{&l};
+    auto res = f.execute();
 
     ASSERT_EQ(res->get_indicator(), "bool");
     ASSERT_FALSE(dynamic_cast<boolean*>(res)->val());
 }
 
 TEST(IsNullTest, should_throw_exception_when_param_passing_to_is_null_is_not_a_list) {
-    auto a1 = new atom{"a1"};
+    auto a1 = atom{"a1"};
 
-    ASSERT_ANY_THROW(new is_null{a1});
+    ASSERT_ANY_THROW(is_null{&a1});
 }
 
 TEST(AddTupleTest, should_get_correct_basic_info_after_successfully_init_add_tuple_expression) {
-    auto l = new list<integer>{};
-    auto i1 = new integer{1};
-    l->push_back(i1);
+    auto l = list<integer>{};
+    auto i1 = integer{1};
+    l.push_back(&i1);
 
-    auto f = new add_tuple{l};
+    auto f = add_tuple{&l};
 
-    ASSERT_EQ(f->name(), "addtup");
-    ASSERT_EQ(f->return_type(), "integer");
-    ASSERT_EQ(f->family(), "list");
+    ASSERT_EQ(f.name(), "addtup");
+    ASSERT_EQ(f.return_type(), "integer");
+    ASSERT_EQ(f.family(), "list");
 }
 
 TEST(AddTupleTest, should_return_0_when_add_tuple_get_empty_tuple) {
-    auto l = new list<integer>{};
+    auto l = list<integer>{};
 
-    auto f = new add_tuple{l};
-    auto res = f->execute();
+    auto f = add_tuple{&l};
+    auto res = f.execute();
 
     ASSERT_EQ(res->get_indicator(), "integer");
     ASSERT_EQ(dynamic_cast<integer*>(res)->val(), 0);
 }
 
 TEST(AddTupleTest, should_return_10_when_add_tuple_get_1_2_3_4_tuple) {
-    auto l = new list<integer>{};
-    auto i1 = new integer{1};
-    auto i2 = new integer{2};
-    auto i3 = new integer{3};
-    auto i4 = new integer{4};
-    l->push_back(i1);
-    l->push_back(i2);
-    l->push_back(i3);
-    l->push_back(i4);
+    auto l = list<integer>{};
+    auto i1 = integer{1};
+    auto i2 = integer{2};
+    auto i3 = integer{3};
+    auto i4 = integer{4};
+    l.push_back(&i1);
+    l.push_back(&i2);
+    l.push_back(&i3);
+    l.push_back(&i4);
 
-    auto f = new add_tuple{l};
-    auto res = f->execute();
+    auto f = add_tuple{&l};
+    auto res = f.execute();
 
     ASSERT_EQ(res->get_indicator(), "integer");
     ASSERT_EQ(dynamic_cast<integer*>(res)->val(), 10);
 }
 
 TEST(AddTupleTest, should_throw_exception_when_getting_an_atom_in_add_tuple) {
-    auto a1 = new atom{"a1"};
+    auto a1 = atom{"a1"};
 
-    ASSERT_ANY_THROW(new add_tuple{a1});
+    ASSERT_ANY_THROW(add_tuple{&a1});
 }
 
 TEST(AddTupleTest, should_throw_exception_when_getting_an_integer_in_add_tuple) {
-    auto i1 = new integer{1};
+    auto i1 = integer{1};
 
-    ASSERT_ANY_THROW(new add_tuple{i1});
+    ASSERT_ANY_THROW(add_tuple{&i1});
 }
 
 TEST(AddTupleTest, should_throw_exception_when_getting_an_atom_list_in_add_tuple) {
-    auto l = new list<atom>{};
-    auto a1 = new atom{"a1"};
-    l->push_back(a1);
+    auto l = list<atom>{};
+    auto a1 = atom{"a1"};
+    l.push_back(&a1);
 
-    ASSERT_ANY_THROW(new add_tuple{l});
+    ASSERT_ANY_THROW(add_tuple{&l});
 }
