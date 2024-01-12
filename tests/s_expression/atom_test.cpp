c0985c44dc1162d303a958912c7a2c6b88a962ba
Edward NoaLand <edwardnoaland@gmail.com>
2024-01-12 09:49:03 +0800

    refactor: remove new operator

--- tests/s_expression/atom_test.cpp
+++ tests/s_expression/atom_test.cpp
@@ -3,77 +3,77 @@
 #include "s_expression/atom.h"
 
 TEST(AtomTest, should_get_value_successfully_after_create_atom) {
-    auto a = new atom{"value_of_atom"};
-    ASSERT_EQ(a->get_value(), "value_of_atom");
-    ASSERT_EQ(a->get_indicator(), "atom");
+    auto a = atom{"value_of_atom"};
+    ASSERT_EQ(a.get_value(), "value_of_atom");
+    ASSERT_EQ(a.get_indicator(), "atom");
 
     std::ostringstream buf;
-    a->print(buf);
+    a.print(buf);
     ASSERT_EQ(buf.str(), "atom: value_of_atom\n");
 }
 
 TEST(BooleanTest, should_set_true_successfully_when_pass_bool_true) {
-    auto b = new boolean{true};
-    ASSERT_TRUE(b->val());
-    ASSERT_EQ(b->get_value(), "#t");
-    ASSERT_EQ(b->get_indicator(), "bool");
+    auto b = boolean{true};
+    ASSERT_TRUE(b.val());
+    ASSERT_EQ(b.get_value(), "#t");
+    ASSERT_EQ(b.get_indicator(), "bool");
 
     std::ostringstream buf;
-    b->print(buf);
+    b.print(buf);
     ASSERT_EQ(buf.str(), "bool: #t\n");
 }
 
 TEST(BooleanTest, should_set_false_successfully_when_pass_bool_false) {
-    auto b = new boolean{false};
+    auto b = boolean{false};
 
-    ASSERT_FALSE(b->val());
-    ASSERT_EQ(b->get_value(), "#f");
-    ASSERT_EQ(b->get_indicator(), "bool");
+    ASSERT_FALSE(b.val());
+    ASSERT_EQ(b.get_value(), "#f");
+    ASSERT_EQ(b.get_indicator(), "bool");
 
     std::ostringstream buf;
-    b->print(buf);
+    b.print(buf);
     ASSERT_EQ(buf.str(), "bool: #f\n");
 }
 
 TEST(BooleanTest, should_set_true_when_pass_string_t) {
     const std::string true_string = "#t";
-    auto b = new boolean{true_string};
+    auto b = boolean{true_string};
 
-    ASSERT_TRUE(b->val());
-    ASSERT_EQ(b->get_value(), "#t");
-    ASSERT_EQ(b->get_indicator(), "bool");
+    ASSERT_TRUE(b.val());
+    ASSERT_EQ(b.get_value(), "#t");
+    ASSERT_EQ(b.get_indicator(), "bool");
 }
 
 TEST(BooleanTest, should_set_true_when_pass_else) {
     const std::string else_string = "else";
-    auto b = new boolean{else_string};
+    auto b = boolean{else_string};
 
-    ASSERT_TRUE(b->val());
-    ASSERT_EQ(b->get_value(), "#t");
-    ASSERT_EQ(b->get_indicator(), "bool");
+    ASSERT_TRUE(b.val());
+    ASSERT_EQ(b.get_value(), "#t");
+    ASSERT_EQ(b.get_indicator(), "bool");
 }
 
 TEST(BooleanTest, should_set_false_when_pass_string_f) {
     const std::string false_string = "#f";
-    auto b = new boolean{false_string};
+    auto b = boolean{false_string};
 
-    ASSERT_FALSE(b->val());
-    ASSERT_EQ(b->get_value(), "#f");
-    ASSERT_EQ(b->get_indicator(), "bool");
+    ASSERT_FALSE(b.val());
+    ASSERT_EQ(b.get_value(), "#f");
+    ASSERT_EQ(b.get_indicator(), "bool");
 }
 
 TEST(BooleanTest, should_throw_exception_when_input_is_not_t_or_else_or_f) {
     const std::string err_string = "err";
-    ASSERT_ANY_THROW(new boolean{err_string});
+    ASSERT_ANY_THROW(boolean{err_string});
 }
 
 TEST(IntegerTest, should_set_10_to_integer_when_passing_10) {
-    auto i = new integer{10};
-    ASSERT_EQ(i->val(), 10);
-    ASSERT_EQ(i->get_value(), "10");
-    ASSERT_EQ(i->get_indicator(), "integer");
+    auto i = integer{10};
+    ASSERT_EQ(i.val(), 10);
+    ASSERT_EQ(i.get_value(), "10");
+    ASSERT_EQ(i.get_indicator(), "integer");
 
     std::ostringstream buf;
-    i->print(buf);
+    i.print(buf);
     ASSERT_EQ(buf.str(), "integer: 10\n");
 }
