eca28c3fd9bb9057df7ef84aff8622b9fc237b5d
Edward NoaLand <edwardnoaland@gmail.com>
2024-01-12 10:20:07 +0800

    refactor: remove new operator

--- tests/s_expression/list_test.cpp
+++ tests/s_expression/list_test.cpp
@@ -4,85 +4,85 @@
 #include "s_expression/list.h"
 
 TEST(ListTest, should_return_tuple_when_list_is_empty) {
-    auto l = new list<atom>{};
+    auto l = list<atom>{};
 
-    ASSERT_EQ(l->get_indicator(), "tuple");
+    ASSERT_EQ(l.get_indicator(), "tuple");
 }
 
 TEST(ListTest, should_return_3_when_list_has_3_value) {
-    auto a1 = new atom{"a1"};
-    auto a2 = new atom{"a2"};
-    auto a3 = new atom{"a3"};
-    auto l = new list<atom>{};
+    auto a1 = atom{"a1"};
+    auto a2 = atom{"a2"};
+    auto a3 = atom{"a3"};
+    auto l = list<atom>{};
 
-    l->push_back(a1);
-    l->push_back(a2);
-    l->push_back(a3);
+    l.push_back(&a1);
+    l.push_back(&a2);
+    l.push_back(&a3);
 
-    ASSERT_EQ(l->size_of(), 3);
+    ASSERT_EQ(l.size_of(), 3);
 }
 
 TEST(ListTest, should_return_true_when_list_is_empty) {
-    auto l = new list<s_expression>{};
+    auto l = list<s_expression>{};
 
-    ASSERT_TRUE(l->empty());
+    ASSERT_TRUE(l.empty());
 
     std::ostringstream buf;
-    l->print(buf);
+    l.print(buf);
     ASSERT_EQ(buf.str(), "tuple: ( )\n");
 }
 
 TEST(ListTest, should_return_false_when_list_has_value) {
-    auto a1 = new atom{"a1"};
-    auto l = new list<atom>{};
+    auto a1 = atom{"a1"};
+    auto l = list<atom>{};
 
-    l->push_back(a1);
+    l.push_back(&a1);
 
-    ASSERT_FALSE(l->empty());
+    ASSERT_FALSE(l.empty());
 }
 
 TEST(ListTest, should_contain_a1_when_already_push_into_it) {
-    auto a1 = new atom{"a1"};
-    auto l = new list<atom>{};
+    auto a1 = atom{"a1"};
+    auto l = list<atom>{};
 
-    l->push_back(a1);
+    l.push_back(&a1);
 
-    ASSERT_TRUE(l->has_value("a1"));
+    ASSERT_TRUE(l.has_value("a1"));
 }
 
 TEST(ListTest, should_return_not_contain_when_a1_is_not_in_list) {
-    auto l = new list<atom>{};
+    auto l = list<atom>{};
 
-    ASSERT_FALSE(l->has_value("a1"));
+    ASSERT_FALSE(l.has_value("a1"));
 }
 
 TEST(ListTest, should_return_value_of_list_successfully_after_input_atoms) {
-    auto a1 = new atom{"a1"};
-    auto a2 = new atom{"a2"};
-    auto a3 = new atom{"a3"};
-    auto l = new list<atom>{};
+    auto a1 = atom{"a1"};
+    auto a2 = atom{"a2"};
+    auto a3 = atom{"a3"};
+    auto l = list<atom>{};
 
-    l->push_back(a1);
-    l->push_back(a2);
-    l->push_back(a3);
+    l.push_back(&a1);
+    l.push_back(&a2);
+    l.push_back(&a3);
 
-    ASSERT_EQ(l->get_value(), "( a1 a2 a3 )");
-    ASSERT_EQ(l->get_indicator(), "list");
+    ASSERT_EQ(l.get_value(), "( a1 a2 a3 )");
+    ASSERT_EQ(l.get_indicator(), "list");
 
     std::ostringstream buf;
-    l->print(buf);
+    l.print(buf);
     ASSERT_EQ(buf.str(), "list: ( a1 a2 a3 )\n");
 }
 
 TEST(ListTest, should_return_iterator_successfully_when_get_iterator) {
-    auto a1 = new atom{"a1"};
-    auto a2 = new atom{"a2"};
-    auto a3 = new atom{"a3"};
-    auto l = new list<atom>{};
+    auto a1 = atom{"a1"};
+    auto a2 = atom{"a2"};
+    auto a3 = atom{"a3"};
+    auto l = list<atom>{};
 
-    l->push_back(a1);
-    l->push_back(a2);
-    l->push_back(a3);
+    l.push_back(&a1);
+    l.push_back(&a2);
+    l.push_back(&a3);
 
-    ASSERT_EQ(l->get_iterator().size(), 3);
+    ASSERT_EQ(l.get_iterator().size(), 3);
 }
