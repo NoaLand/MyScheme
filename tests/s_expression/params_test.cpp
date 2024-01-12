98ebf2da7a437aa22130c7e66cac49be3cfb38b6
Edward NoaLand <edwardnoaland@gmail.com>
2024-01-12 10:18:18 +0800

    refactor: remove new operator

--- tests/s_expression/params_test.cpp
+++ tests/s_expression/params_test.cpp
@@ -3,11 +3,11 @@
 #include "s_expression/params.h"
 
 TEST(ParamsTest, should_get_value_successfully_after_create_param) {
-    auto p = new param{"value_of_param"};
-    ASSERT_EQ(p->get_value(), "value_of_param");
-    ASSERT_EQ(p->get_indicator(), "param");
+    auto p = param{"value_of_param"};
+    ASSERT_EQ(p.get_value(), "value_of_param");
+    ASSERT_EQ(p.get_indicator(), "param");
 
     std::ostringstream buf;
-    p->print(buf);
+    p.print(buf);
     ASSERT_EQ(buf.str(), "param: value_of_param\n");
 }
