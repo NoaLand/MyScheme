4c831dee9f1cdfac70d9bb8c96cc71e521a839a8
Edward NoaLand <edwardnoaland@gmail.com>
2024-01-12 10:13:38 +0800

    refactor: remove new operator using of allocating memory

--- tests/function/s_expression_family_test.cpp
+++ tests/function/s_expression_family_test.cpp
@@ -5,68 +5,68 @@
 #include "function/others.h"
 
 TEST(IsAtomTest, should_get_basic_info_when_successfully_create_is_atom_expression) {
-    auto a = new atom{"a"};
-    auto f = new is_atom{a};
+    auto a = atom{"a"};
+    auto f = is_atom{&a};
 
-    ASSERT_EQ(f->name(), "atom?");
-    ASSERT_EQ(f->return_type(), "bool");
-    ASSERT_EQ(f->family(), "s_expression");
+    ASSERT_EQ(f.name(), "atom?");
+    ASSERT_EQ(f.return_type(), "bool");
+    ASSERT_EQ(f.family(), "s_expression");
 }
 
 TEST(IsAtomTest, should_return_true_when_input_is_an_atom) {
-    auto a = new atom{"a"};
+    auto a = atom{"a"};
 
-    auto f = new is_atom{a};
-    auto res = f->execute();
+    auto f = is_atom{&a};
+    auto res = f.execute();
 
     ASSERT_EQ(res->get_indicator(), "bool");
     ASSERT_TRUE(res->val());
 }
 
 TEST(IsAtomTest, should_return_true_when_input_is_a_boolean) {
-    auto b = new boolean{true};
+    auto b = boolean{true};
 
-    auto f = new is_atom{b};
-    auto res = f->execute();
+    auto f = is_atom{&b};
+    auto res = f.execute();
 
     ASSERT_EQ(res->get_indicator(), "bool");
     ASSERT_TRUE(res->val());
 }
 
 TEST(IsAtomTest, should_return_true_when_input_is_an_integer) {
-    auto i = new integer{1};
+    auto i = integer{1};
 
-    auto f = new is_atom{i};
-    auto res = f->execute();
+    auto f = is_atom{&i};
+    auto res = f.execute();
 
     ASSERT_EQ(res->get_indicator(), "bool");
     ASSERT_TRUE(res->val());
 }
 
 TEST(IsAtomTest, should_return_false_when_input_is_a_list) {
-    auto l = new list<atom>{};
+    auto l = list<atom>{};
 
-    auto f = new is_atom{l};
-    auto res = f->execute();
+    auto f = is_atom{&l};
+    auto res = f.execute();
 
     ASSERT_EQ(res->get_indicator(), "bool");
     ASSERT_FALSE(res->val());
 }
 
 TEST(IsNumberTest, should_get_basic_info_when_successfully_create_is_number_expression) {
-    auto a = new atom{"a"};
-    auto f = new is_number{a};
+    auto a = atom{"a"};
+    auto f = is_number{&a};
 
-    ASSERT_EQ(f->name(), "number?");
-    ASSERT_EQ(f->return_type(), "bool");
-    ASSERT_EQ(f->family(), "s_expression");
+    ASSERT_EQ(f.name(), "number?");
+    ASSERT_EQ(f.return_type(), "bool");
+    ASSERT_EQ(f.family(), "s_expression");
 }
 
 TEST(IsNumberTest, should_return_true_when_input_is_a_number) {
-    auto i = new integer{1};
+    auto i = integer{1};
 
-    auto f = new is_number{i};
-    auto res = f->execute();
+    auto f = is_number{&i};
+    auto res = f.execute();
 
     ASSERT_EQ(res->get_indicator(), "bool");
     ASSERT_TRUE(res->val());
@@ -74,78 +74,78 @@ TEST(IsNumberTest, should_return_true_when_input_is_a_number) {
 
 
 TEST(IsNumberTest, should_return_false_when_input_is_an_atom) {
-    auto a = new atom{"a"};
+    auto a = atom{"a"};
 
-    auto f = new is_number{a};
-    auto res = f->execute();
+    auto f = is_number{&a};
+    auto res = f.execute();
 
     ASSERT_EQ(res->get_indicator(), "bool");
     ASSERT_FALSE(res->val());
 }
 
 TEST(IsNumberTest, should_return_false_when_input_is_a_list) {
-    auto l = new list<atom>{};
+    auto l = list<atom>{};
 
-    auto f = new is_number{l};
-    auto res = f->execute();
+    auto f = is_number{&l};
+    auto res = f.execute();
 
     ASSERT_EQ(res->get_indicator(), "bool");
     ASSERT_FALSE(res->val());
 }
 
 TEST(IsEqTest, should_get_basic_info_when_successfully_create_is_eq_expression) {
-    auto l = new atom{"l"};
-    auto r = new atom{"r"};
-    auto f = new is_eq{l, r};
+    auto l = atom{"l"};
+    auto r = atom{"r"};
+    auto f = is_eq{&l, &r};
 
-    ASSERT_EQ(f->name(), "eq?");
-    ASSERT_EQ(f->return_type(), "bool");
-    ASSERT_EQ(f->family(), "s_expression");
+    ASSERT_EQ(f.name(), "eq?");
+    ASSERT_EQ(f.return_type(), "bool");
+    ASSERT_EQ(f.family(), "s_expression");
 }
 
 TEST(IsEqTest, should_return_true_when_l_and_r_are_same_in_type_and_value) {
-    auto l = new atom{"a1"};
-    auto r = new atom{"a1"};
+    auto l = atom{"a1"};
+    auto r = atom{"a1"};
 
-    auto f = new is_eq{l, r};
-    auto res = f->execute();
+    auto f = is_eq{&l, &r};
+    auto res = f.execute();
 
     ASSERT_EQ(res->get_indicator(), "bool");
     ASSERT_TRUE(res->val());
 }
 
 TEST(IsEqTest, should_return_false_when_l_is_atom_and_r_is_list_of_l) {
-    auto l = new atom{"a"};
+    auto l = atom{"a"};
 
-    auto r = new list<atom>{};
-    r->push_back(l);
+    auto r = list<atom>{};
+    r.push_back(&l);
 
-    auto f = new is_eq{l, r};
-    auto res = f->execute();
+    auto f = is_eq{&l, &r};
+    auto res = f.execute();
 
     ASSERT_EQ(res->get_indicator(), "bool");
     ASSERT_FALSE(res->val());
 }
 
 TEST(IsEqTest, should_return_false_when_l_is_atom_and_r_is_bool) {
-    auto l = new atom{"a"};
-    auto r = new boolean{true};
+    auto l = atom{"a"};
+    auto r = boolean{true};
 
-    auto f = new is_eq{l, r};
-    auto res = f->execute();
+    auto f = is_eq{&l, &r};
+    auto res = f.execute();
 
     ASSERT_EQ(res->get_indicator(), "bool");
     ASSERT_FALSE(res->val());
 }
 
 TEST(IsEqTest, should_return_false_when_l_is_true_and_r_is_quote_true) {
-    auto l = new boolean{true};
+    auto l = boolean{true};
 
-    auto quote_func = new quote{l};
-    auto r = quote_func->execute();
+    auto quote_func = quote{&l};
+    auto r = quote_func.execute();
 
-    auto f = new is_eq{l, r};
-    auto res = f->execute();
+    auto f = is_eq{&l, r};
+    auto res = f.execute();
 
     ASSERT_EQ(res->get_indicator(), "bool");
     ASSERT_FALSE(res->val());
